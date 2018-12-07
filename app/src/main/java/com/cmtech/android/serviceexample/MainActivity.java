d1f5ef472a57f59801855638ecffb7abd568b604
chenmtech <1276094137@qq.com>
2018-12-07 10:47:09 +0800

    创建绑定服务；

--- app/src/main/java/com/cmtech/android/serviceexample/MainActivity.java
+++ app/src/main/java/com/cmtech/android/serviceexample/MainActivity.java
@@ -1,17 +1,35 @@
 package com.cmtech.android.serviceexample;
 
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.cmtech.android.serviceexample.service.LocalService;
 import com.cmtech.android.serviceexample.service.SimpleService;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
     private Button btnStartService;
     private Button btnStopService;
 
+    Button btnBind;
+    Button btnUnBind;
+    Button btnGetDatas;
+    /**
+     * ServiceConnection代表与服务的连接，它只有两个方法，
+     * onServiceConnected和onServiceDisconnected，
+     * 前者是在操作者在连接一个服务成功时被调用，而后者是在服务崩溃或被杀死导致的连接中断时被调用
+     */
+    private ServiceConnection conn;
+    private LocalService mService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +53,70 @@ public class MainActivity extends AppCompatActivity {
                 stopService(intent);
             }
         });
+
+        btnBind = (Button) findViewById(R.id.BindService);
+        btnUnBind = (Button) findViewById(R.id.unBindService);
+        btnGetDatas = (Button) findViewById(R.id.getServiceDatas);
+        //创建绑定对象
+        final Intent intent = new Intent(this, LocalService.class);
+
+        // 开启绑定
+        btnBind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "绑定调用：bindService");
+                //调用绑定方法
+                bindService(intent, conn, Service.BIND_AUTO_CREATE);
+            }
+        });
+        // 解除绑定
+        btnUnBind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "解除绑定调用：unbindService");
+                // 解除绑定
+                if(mService!=null) {
+                    mService = null;
+                    unbindService(conn);
+                }
+            }
+        });
+
+        // 获取数据
+        btnGetDatas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mService != null) {
+                    // 通过绑定服务传递的Binder对象，获取Service暴露出来的数据
+                    Log.i(TAG, "从服务端获取数据：" + mService.getCount());
+                } else {
+                    Log.i(TAG, "还没绑定呢，先绑定,无法从服务端获取数据");
+                }
+            }
+        });
+
+
+        conn = new ServiceConnection() {
+            /**
+             * 与服务器端交互的接口方法 绑定服务的时候被回调，在这个方法获取绑定Service传递过来的IBinder对象，
+             * 通过这个IBinder对象，实现宿主和Service的交互。
+             */
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                Log.i(TAG, "绑定成功调用：onServiceConnected");
+                // 获取Binder
+                LocalService.LocalBinder binder = (LocalService.LocalBinder) service;
+                mService = binder.getService();
+            }
+            /**
+             * 当取消绑定的时候被回调。但正常情况下是不被调用的，它的调用时机是当Service服务被意外销毁时，
+             * 例如内存的资源不足时这个方法才被自动调用。
+             */
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                Log.i(TAG, "解除绑定调用：onServiceDisconnected");
+                mService=null;
+            }
+        };
     }
 }
